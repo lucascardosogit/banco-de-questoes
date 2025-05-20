3aadb5d45f5a5360433e59846602ed12744de0db
lucascardosogit <lucasepcardoso@hotmail.com>
2025-05-20 18:21:21 -0300

    Ajustes em estrutura, nomenclatura e estilo HTML e CSS, além de lógica JS

--- script.js
+++ script.js
@@ -1,35 +1,109 @@
-const container = document.getElementById("listaQuestoes");
+let questions = JSON.parse(localStorage.getItem("questions")) || [];
 
-function carregarQuestoes() {
-  const questoes = JSON.parse(localStorage.getItem("questoes")) || [];
-  container.innerHTML = "";
+const matters = ["Programação Web", "Banco de Dados", "Algoritmos"];
+const contents = ["HTML", "CSS", "JavaScript"];
+const course = ["Análise e Desenvolvimento de Sistemas", "Engenharia de Software", "Sistemas de Informação", "Redes de Computadores"];
 
-  if (questoes.length === 0) {
-    container.innerHTML = "<p>Nenhuma questão cadastrada.</p>";
+// Lógica página de cadastro de questões
+function questRegLoader() {
+  optionsLoad('#quest-matter', matters, 'Selecione uma matéria');
+  optionsLoad('#quest-content', contents, 'Selecione um conteúdo');
+}
+
+function optionsLoad(position, arrayOfContent, placeholderMsg) {
+  const element = document.querySelector(position);
+
+  const elementPreSet = document.createElement("option");
+  elementPreSet.text =  placeholderMsg.toString();
+  elementPreSet.selected = true;
+  elementPreSet.disabled = true;
+  element.appendChild(elementPreSet)
+  arrayOfContent.forEach((el) => {
+    let tempElement = document.createElement("option");
+    tempElement.text = el.toString();
+    element.appendChild(tempElement);
+  })
+}
+
+function setNewQuest() {
+  const matter = document.getElementById("quest-matter").value.trim();
+  const content = document.getElementById("quest-content").value.trim();
+  const questHeader = document.getElementById("quest-header").value.trim();
+  const alternativeInputs = document.querySelectorAll(".alternative-input");
+  const correctAltIndex = document.querySelector('input[name="isCorrect"]:checked').value;
+
+  if (!matter || !content || !questHeader || !alternativeInputs) {
+    alert("Preencha todos os campos e selecione a opção correta.");
+  return;
+  }
+
+  const alternatives = Array.from(alternativeInputs).map(alt => alt.value.trim());
+
+  const newQuest = {
+    questNumberID: questions.length + 1,
+    matter,
+    content,
+    questHeader,
+    alternativesArray: alternatives,
+    correctAltIndex
+  };
+
+  questions.push(newQuest);
+
+  localStorage.setItem("questions", JSON.stringify(questions));
+
+  alert(`Questão ${newQuest.questNumberID} adicionada com sucesso!`);
+
+  document.getElementById("quest-form").reset();
+}
+
+// Lógica página gerar prova
+
+function testGenLoader() {
+  questionsLoader();
+  optionsLoad('#test-course', course, 'Selecione a cadeira');
+  optionsLoad('#test-matter', matters, 'Selecione uma matéria');
+  optionsLoad('#test-content', contents, 'Selecione um conteúdo');
+}
+
+function questionsLoader() {
+  const questionsContainer = document.getElementById("quest-list-container");
+
+  questions = JSON.parse(localStorage.getItem("questions")) || [];
+
+  if (questions.length === 0) {
+    questionsContainer.innerHTML = "<p>Nenhuma questão cadastrada.</p>";
   } else {
-    questoes.forEach((questao, index) => {
-      const cardId = `card${index + 1}`;
-      const checkboxId = `q${index + 1}`;
+    questions.forEach((quest, index) => {
+      const questCardID = `quest-card-${index + 1}`;
+      const questCardCheckboxID = `quest-checkbox-${index + 1}`;
       
-      const card = document.createElement("div");
-      card.className = "question-card";
-      card.id = cardId;
+      const questCard = document.createElement("div");
+      questCard.className = "question-card";
+      questCard.id = questCardID;
       
-      const opcoesList = questao.opcoes.map((opcao, idx) => {
-        const letra = String.fromCharCode(97 + idx);
-        const isCorreta = idx === questao.indexCorreta;
-        return `<li ${isCorreta ? 'style="font-weight:bold; color:green;"' : ''}>${letra}) ${opcao}</li>`;
+      const alternativeList = quest.alternativesArray.map((alt, index) => {
+        const altID = String.fromCharCode(97 + index);
+
+        if(parseInt(quest.correctAltIndex) === index) {
+          return `<li class="correct-alternative">${altID}) ${alt}</li>`;
+        } else {
+          return `<li>${altID}) ${alt}</li>`;
+        }
       }).join("");
       
-      card.innerHTML = 
-      `<div class="questao cabecalho">
-      <h6>Questão ${questao.numeroQuestao}</h6>
-      <input type="checkbox" role="switch" id="${checkboxId}" onclick="toggleQuestion('${checkboxId}', '${cardId}')">
+      questCard.innerHTML = 
+      `<div class="question-header">
+        <div class="quest-matter-content">
+          <p>Matéria: ${quest.matter}</p>
+          <p>Conteúdo: ${quest.content}</p>
+        </div>
+        <input type="checkbox" role="switch" id="${questCardCheckboxID}" onclick="toggleQuestion('${questCardCheckboxID}', '${questCardID}')">
       </div>
-      <p>${questao.enunciado}</p>
-      <ul>${opcoesList}</ul>`;
+      <p>${quest.questHeader}</p>
+      <ul>${alternativeList}</ul>`;
       
-      container.appendChild(card);
+      questionsContainer.appendChild(questCard);
     });
   }
 }
@@ -44,51 +118,19 @@ function toggleQuestion(checkboxId, cardId) {
   }
 }
 
-function adicionarQuestao() {
-  const materia = document.getElementById("subject").value.trim();
-  const conteudo = document.getElementById("content").value.trim();
-  const enunciado = document.getElementById("quest-header").value.trim();
-  const opcaoInputs = document.querySelectorAll(".alternative-input");
-  const questCorreta = document.querySelector('input[name="isCorrect"]:checked');
-
-  if (!materia || !conteudo || !enunciado || !questCorreta) {
-    alert("Preencha todos os campos e selecione a opção correta.");
-  return;
-  }
-
-  const opcoes = Array.from(opcaoInputs).map(input => input.value.trim());
-
-  const indexCorreta = parseInt(questCorreta.value);
-
-  let questoes = JSON.parse(localStorage.getItem("questoes")) || [];
-
-  const novaQuestao = {
-    numeroQuestao: questoes.length + 1,
-    materia,
-    conteudo,
-    enunciado,
-    opcoes,
-    indexCorreta
-  };
-
-  questoes.push(novaQuestao);
-  localStorage.setItem("questoes", JSON.stringify(questoes));
-
-  alert(`Questão ${novaQuestao.numeroQuestao} adicionada com sucesso!`);
-
-  document.getElementById("quest-form").reset();
-}
-
 function gerarProva() {
+  event.preventDefault();
   
-  let questoes = JSON.parse(localStorage.getItem("questoes")) || [];
+  let questions = JSON.parse(localStorage.getItem("questions")) || [];
 
-  if (questoes.length === 0) {
-    alert("Você não possui questões cadastradas. Cadastre-as para gerar provas");
+  if (questions.length === 0) {
+    alert("Você não possui questões cadastradas. Cadastre-as para gerar provas!");
     return;
   }
 
   const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  console.log(checkboxes);
+
 
   if(checkboxes.length === 0) {
     alert("Selecione pelo menos uma questão para gerar a prova.");
@@ -97,10 +139,10 @@ function gerarProva() {
 
   const questoesSelecionadas = Array.from(checkboxes).map(checkbox => {
     const cardId = checkbox.id.replace('q', '');
-    return questoes[parseInt(cardId) - 1];
+    return questions[parseInt(cardId) - 1];
   });
 
-  const variacoesInput = parseInt(document.getElementById("variacoes").value);
+  const variacoesInput = parseInt(document.getElementById("tests-variation").value);
 
   if (isNaN(variacoesInput) || variacoesInput <= 0) {
       alert("Informe um número válido de variações.");
@@ -128,7 +170,7 @@ function gerarProva() {
 
 function carregarProvas() {
   const variacoes = JSON.parse(localStorage.getItem("variacoes")) || [];
-  const containerProvas = document.getElementById("listaProvas");
+  const containerProvas = document.getElementById("tests-list");
   containerProvas.innerHTML = "";
 
   if (variacoes.length === 0) {
@@ -172,7 +214,7 @@ function carregarProvas() {
   }
 }
 
-function limparLocal(){
+function clearLocalStorage() {
   localStorage.clear();
-  alert('Questões excluidas com sucesso!');
+  alert("Banco de questões zerado com sucesso!");
 }
